b050e30d02779441594da19ace2bc4a7e8216666
Hilde Weerts <hilde_weerts@hotmail.com>
2020-05-12 14:19:43 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="camelbird", # Replace with your own username
+    version="0.0.1",
+    description="Fair Machine Learning",
+    url="https://github.com/hildeweerts/camelbird",
+    author="Hilde Weerts",
+    author_email="githubspam@hildeweerts.nl",
+    licence="BSD 3-Clause License",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy",
+                      "scikit-learn",
+                      ],
+    python_requires='>=3.6',
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
+)
